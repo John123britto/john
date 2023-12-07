d45649ef938052f1d241d54b9d647375197ef071
John123britto <153165165+John123britto@users.noreply.github.com>
2023-12-07 12:08:54 +0530

    Add files via upload

---
+++ pyex1.py
@@ -0,0 +1,38 @@
+print("STRING FUNCTION\n"\
+       "1 len\n"\
+       "2 Lower\n"\
+       "3 islower\n"\
+       "4 title\n"\
+       "5 isdecimal\n"\
+       "6 isupper\n"\
+       "7 capitalize\n"\
+       "8 isnumeric\n"\
+       "9 isalpha\n"\
+       "10 upper\n")
+
+while true:
+    a=int(input("Enter a number between 1 to 10"))
+    b=input("Enter a string")
+    if(a==1):
+        print(a.swapcase())
+    elif(a==2):
+         print(a.swapcase())
+    elif(a==2):
+         print(a.swapcase())
+    elif(a==2):
+         print(a.swapcase())
+    elif(a==2):
+         print(a.swapcase())
+    elif(a==2):
+         print(a.swapcase())
+    elif(a==2):
+         print(a.swapcase())
+    elif(a==2):
+         print(a.swapcase())
+    elif(a==2):
+         print(a.swapcase())
+    elif(a==2):
+         print(a.swapcase())
+    else:
+        print(please enter)
+    
